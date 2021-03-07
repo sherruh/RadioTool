536f07e37011b9445a02fc4a256f30d6255356b4
sherruh <sherruh@gmail.com>
2021-03-08 02:27:22 +0600

    start moving params to ParamsFragment

--- app/src/main/java/com/example/radiotestapp/youtube_params/YoutubeParamsFragment.java
+++ app/src/main/java/com/example/radiotestapp/youtube_params/YoutubeParamsFragment.java
@@ -5,17 +5,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.radiotestapp.R;
 import com.example.radiotestapp.main.MainViewModel;
+import com.example.radiotestapp.utils.Toaster;
 
 public class YoutubeParamsFragment extends Fragment {
 
     private MainViewModel mViewModel;
+    private TextView textInitializationTime;
+    private TextView textBufferingTime;
 
     public YoutubeParamsFragment(MainViewModel mainViewModel) {
         mViewModel = mainViewModel;
@@ -40,6 +45,30 @@ public class YoutubeParamsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initViews(view);
+        initViewModel();
+    }
 
+    private void initViewModel() {
+        mViewModel.initTimeLiveData.observe(getViewLifecycleOwner(), s ->{ textInitializationTime
+                .setText(String.valueOf(s / 1000.0));});
+        mViewModel.bufferingTimeLiveData.observe(getViewLifecycleOwner(), s ->{textBufferingTime
+                .setText(String.valueOf(s / 1000.0));});
+        mViewModel.onStartYoutubeClickedEvent.observe(getViewLifecycleOwner(), aVoid -> {
+            textInitializationTime.setText("--");
+            textBufferingTime.setText("--");
+        });
+        mViewModel.loggingStoppedEvent.observe(getViewLifecycleOwner(),aVoid -> {
+            textInitializationTime.setText("--");
+            textBufferingTime.setText("--");
+        });
+    }
+
+    private void initViews(View view) {
+        textInitializationTime = view.findViewById(R.id.text_youtube_init_value_youtube_params_fragment);
+        textBufferingTime = view.findViewById(R.id.text_youtube_buffering_value_youtube_params_fragment);
+        textInitializationTime.setText("--");
+        textBufferingTime.setText("--");
+        Toaster.showShort(getContext(),"Here");
     }
 }
